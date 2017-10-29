0bb3d9b1c699076306d93c4c9fbe0af84257290f
Lukáš Pokorný <luk4s.pokorny@gmail.com>
2017-10-29 14:38:46 +0100

    migrations to app

--- lib/mautic/engine.rb
+++ lib/mautic/engine.rb
@@ -5,5 +5,14 @@ module Mautic
     config.generators do |g|
       g.test_framework :rspec, fixture: false
     end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths['db/migrate'].expanded.each do |expanded_path|
+          app.config.paths['db/migrate'] << expanded_path
+        end
+      end
+    end
+
   end
 end
