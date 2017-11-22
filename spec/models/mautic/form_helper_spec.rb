4ff97318d434a27e9781da96dfbef9b3e9894f82
Lukáš Pokorný <luk4s.pokorny@gmail.com>
2017-11-22 22:20:02 +0100

    submit mautic form always return 302

--- spec/models/mautic/form_helper_spec.rb
+++ spec/models/mautic/form_helper_spec.rb
@@ -6,7 +6,7 @@ module Mautic
     it '#send_data' do
       m = FormHelper.new("https://mautic.fake.com")
       stub = stub_request(:post, "#{m.url}/form/submit")
-               .with(body: hash_including("mauticform" => hash_including("name"))).to_return(status: 200)
+               .with(body: hash_including("mauticform" => hash_including("name"))).to_return(status: 302)
 
       m.send_data 1 do |i|
         i.name = Faker::Name.first_name
@@ -18,7 +18,7 @@ module Mautic
     it '#push' do
       m = FormHelper.new("https://mautic.fake.com")
       stub = stub_request(:post, "#{m.url}/form/submit")
-               .with(body: hash_including("mauticform" => hash_including("name"))).to_return(status: 200)
+               .with(body: hash_including("mauticform" => hash_including("name"))).to_return(status: 302)
       m.data = {name: Faker::Name.first_name, email: Faker::Internet.free_email}
       m.submit
       expect(stub).to have_been_made
@@ -26,7 +26,7 @@ module Mautic
 
     it '.submit' do
       stub = stub_request(:post, "https://mautic.my.app/form/submit")
-               .with(body: hash_including("mauticform" => hash_including("name", "domain"))).to_return(status: 200)
+               .with(body: hash_including("mauticform" => hash_including("name", "domain"))).to_return(status: 302)
 
       FormHelper.submit(form: 13, request: OpenStruct.new(host: Faker::Internet.domain_name)) do |i|
         i.name = Faker::Name.first_name
